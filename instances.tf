6c6782a15a5d9058d1dd05b3c21552fa5058b031
Peter Jadesimi <Peter.Jadesimi@ngkf.com>
2020-11-17 20:29:09 -0500

    all terraform scripts

---
+++ instances.tf
@@ -0,0 +1,30 @@
+
+#Get Linux AMI ID using SSM Parameter endpoint in us-east-1
+data "aws_ssm_parameter" "linuxAmi" {
+  provider = aws.region-master
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+#Get Linux AMI ID using SSM Parameter endpoint in us-west-2
+data "aws_ssm_parameter" "linuxAmiOregon" {
+  provider = aws.region-worker
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+
+#Please note that this code expects SSH key pair to exist in default dir under
+#users home directory, otherwise it will fail
+
+#Create key-pair for logging into EC2 in us-east-1
+resource "aws_key_pair" "master-key" {
+  provider   = aws.region-master
+  key_name   = "jenkins"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+#Create key-pair for logging into EC2 in us-west-2
+resource "aws_key_pair" "worker-key" {
+  provider   = aws.region-worker
+  key_name   = "jenkins"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+
+variable "profile" {
+        type = string
+        default = "default"
+}
+
+variable "region-master" {
+        type = string
+        default = "us-east-1"
+}
+
+variable "region-worker" {
+        type = string
+        default = "us-west-2"
+}
+
+variable "external_ip" {
+        type = string
+        default = "0.0.0.0/0"
+}
+
+#This chunk of template can also be put inside variables.tf for better segregation
+
+variable "workers-count" {
+  type    = number
+  default = 1
+}
+
+variable "instance-type" {
+  type    = string
+  default = "t3.micro"
+}
